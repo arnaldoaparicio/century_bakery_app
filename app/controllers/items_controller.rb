a4778b760c3e397dd388244bde490a7ed3723569
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-02-19 17:46:43 -0500

    Add strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
   def new
-    
+
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.permit(:name, :minimum, :number_in_stock, :item_type, :note, :list_id)
   end
 end
