55abc2bc5b5f117e339b5e301a26f2b0b36e4231
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-03-10 18:32:20 -0400

    Add test to check welcome page

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ' Welcome/Index', type: :feature do
+
+    it 'shows the welcome page' do
+    visit '/'
+
+    expect(page).to have_text('Welcome to the Century Checklist.')
+    end
+end
