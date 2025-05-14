810767d4e6a826885b71ab582daf08c65ab62346
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-13 21:09:31 -0400

    Add enum for 'item_type' attribute

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :list
 
+  enum item_type: {
+    ingredient: 0,
+    paper: 1,
+    cleaning: 2
+  }
+
   def need
     minimum - number_in_stock
   end
