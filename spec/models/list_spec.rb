9aa5fdeec7e41e68c254b7548f430fe76c7f7c29
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-21 14:37:22 -0400

    Add test to check validations

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe List, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:items) }
   end
 end
