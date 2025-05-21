4e39d0e85e29158e10e8e649a96a1d2323acfd33
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-21 14:38:27 -0400

    Add test to check for validations of all of 'Item' attributes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Item, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:minimum) }
+    it { should validate_presence_of(:number_in_stock) }
+    it { should validate_presence_of(:item_type) }
+    it { should validate_presence_of(:note) }
   end
   
   describe "instance methods" do
