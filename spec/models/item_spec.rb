5e2f9ac482b51fb003559f05053f5cd2b8ea161f
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-21 14:24:18 -0400

    Add test to check validations for 'Item'

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to(:list) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
   
   describe "instance methods" do
     describe "#need" do
