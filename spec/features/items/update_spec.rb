331d515f817ea345307351106f78ef5867f80349
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-27 13:12:28 -0400

    Update test to use proper path and more form data

--- spec/features/items/update_spec.rb
+++ spec/features/items/update_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe 'the item update' do
   it 'shows the item edit form' do
     ingredient_list = List.create!(name: 'Ingredients')
+    baker_cheese = Item.create!(name: "Baker's Cheese", minimum: 1, number_in_stock: 0, item_type: 0, note: "Must buy 30# box, not 10#", list_id: ingredient_list.id)
 
-    visit "/lists/#{ingredient_list.id}/edit"
+    visit "/lists/#{ingredient_list.id}/#{baker_cheese.id}/edit"
 
     expect(find("form")).to have_content('Name')
+    expect(find('form')).to have_content('Minimum')
+    expect(find('form')).to have_content('# In Stock')
+    expect(find('form')).to have_content('Item Type')
+    expect(find('form')).to have_content('Note')
   end
 end
