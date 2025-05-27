ef2f9953de9d01e3ddc865e45c253910389a3d99
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-05-27 13:42:10 -0400

    Update path

--- spec/features/items/update_spec.rb
+++ spec/features/items/update_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'the item update' do
     ingredient_list = List.create!(name: 'Ingredients')
     baker_cheese = Item.create!(name: "Baker's Cheese", minimum: 1, number_in_stock: 0, item_type: 0, note: "Must buy 30# box, not 10#", list_id: ingredient_list.id)
 
-    visit "/lists/#{ingredient_list.id}/#{baker_cheese.id}/edit"
+    visit "/lists/#{ingredient_list.id}/items/#{baker_cheese.id}/edit"
 
     expect(find("form")).to have_content('Name')
     expect(find('form')).to have_content('Minimum')
