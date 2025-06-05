6307e56317715c29887c91dd017e996e3c77f61d
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-06-04 20:05:09 -0400

    Adjust formatting

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe Item, type: :model do
 
     it { should validate_numericality_of(:minimum) }
     it { should validate_numericality_of(:number_in_stock) }
-    
   end
-  
-  describe "instance methods" do
-    describe "#need" do
-      it "returns the difference between Minimum and in stock" do
-        list = List.create(name: "Ingredients")
-        item = list.items.create(name: "Box of 36 Count Paper Towels", minimum: 4, number_in_stock: 1, item_type: 2, note: "Don't order Brawny brand.",)
+
+  describe 'instance methods' do
+    describe '#need' do
+      it 'returns the difference between Minimum and in stock' do
+        list = List.create(name: 'Ingredients')
+        item = list.items.create(name: 'Box of 36 Count Paper Towels', minimum: 4, number_in_stock: 1, item_type: 2,
+                                 note: "Don't order Brawny brand.")
 
         expect(item.need).to eq(3)
       end
