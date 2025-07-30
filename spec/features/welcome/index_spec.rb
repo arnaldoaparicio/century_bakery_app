334459e16a5a0d2ae327914bd92d36fd62ef4310
Arnaldo Aparicio <apaarn2211@gmail.com>
2025-07-30 18:50:01 -0400

    Adjust formatting

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ' Welcome/Index', type: :feature do
-
-    it 'shows the welcome page' do
+  it 'shows the welcome page' do
     visit '/'
 
     expect(page).to have_text('Welcome to the Century Checklist.')
-    end
+  end
 end
